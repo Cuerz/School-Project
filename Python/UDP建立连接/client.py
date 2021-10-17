f5464fc418ecb8ab7abb200cb219b258ed8da8a1
Sean <84277976+LJH0726@users.noreply.github.com>
2021-10-17 21:58:48 +0800

    Update client.py

--- Python/UDP建立连接/client.py
+++ Python/UDP建立连接/client.py
@@ -6,7 +6,7 @@ def main():
     c = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     while True:
         text = input("-->")
-        c.sendto(text.encode("utf-8"),("10.0.37.69",6000))
+        c.sendto(text.encode("utf-8"),("ip",6000))
         if text == 'exit':
             break
         ans = c.recvfrom(1024)
